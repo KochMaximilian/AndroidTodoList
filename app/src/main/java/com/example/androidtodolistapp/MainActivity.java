603184840b282b51260cb9f47be15a87275f2adf
Maximilian Koch <1710653582@stud.fh-kufstein.ac.at>
2020-01-30 18:37:07 +0100

    new database version and changed the genereted notes from 1000 to 5000

--- app/src/main/java/com/example/androidtodolistapp/MainActivity.java
+++ app/src/main/java/com/example/androidtodolistapp/MainActivity.java
@@ -153,7 +153,7 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewAdapt
     }
 
     private void buildDummyTodos() {
-        for (int i = 1; i <= 1000; i++) {
+        for (int i = 1; i <= 5000; i++) {
             count++;
             Todo todo = new Todo();
             todo.name = "Note " + count;
