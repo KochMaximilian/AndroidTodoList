ba21c0f3ccf408717528c30ddaa16d48d21fcd3d
Maximilian Koch <1710653582@stud.fh-kufstein.ac.at>
2020-01-30 16:30:40 +0100

    changes for testing

--- app/src/main/java/com/example/androidtodolistapp/Database.java
+++ app/src/main/java/com/example/androidtodolistapp/Database.java
@@ -2,7 +2,7 @@ package com.example.androidtodolistapp;
 
 import androidx.room.RoomDatabase;
 
-@androidx.room.Database(entities = {Todo.class}, version = 4, exportSchema = false)
+@androidx.room.Database(entities = {Todo.class}, version = 5, exportSchema = false)
 public abstract class Database extends RoomDatabase {
 
     public static final String DB_NAME = "app_db";
